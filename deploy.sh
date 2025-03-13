ef10fcc102125f40d678f3bbc582129b1ef2dcb4
Osman Uysal <osmanuysalca@outlook.com>
2025-03-13 15:01:44 +0000

    updated config and deploy

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,8 @@ cd SimpleApplication/
 
 # Install application dependencies
 npm install
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
 
 # Start the application using pm2 with the process name simple_app
 pm2 start ./bin/www --name simple_app
